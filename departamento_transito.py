a243088e23d5b9a192841747de7d030fe30abb92
unknown <aluno28017@mtorga.edu.pt>
2025-10-16 11:06:00 +0100

    Escola javali cansado

--- departamento_transito.py
+++ departamento_transito.py
@@ -2,9 +2,8 @@ ano_atual = int(input("Digite o ano Atual : "))
 ano_nasc = int(input("Digite o ano de nascimento : "))
 
 idade = ano_atual - ano_nasc
-print("O ano atual é {} e o ano de seu nascimento é {} então, a sua idade é de {} anos " 
-    .format(ano_atual, ano_nasc, idade))
+print("O ano atual é {} e o ano de seu nascimento é {} então, a sua idade é de {} anos "   .format(ano_atual, ano_nasc, idade))
 if idade >= 18 :
     print("Como você é maior de idade estás apto para conduzir")
 else:
-    print("Como você é menor de idade não estás apto para conduzir, !Alerta cuidado ao dirigir")
+    print("Como você é menor de idade não estás apto para conduzir")
---
+++ escola_javali_cansado.py
@@ -0,0 +1,18 @@
+print("------------------------")
+
+print("ESCOLA JAVALI CANSADO")
+
+print("------------------------")
+n1 = int(input("Primeira Nota : "))
+n2 = int(input("Segunda Nota : "))
+
+print("------------------------")
+
+media = float ((n1 + n2)/2) 
+print("A sua media é de : {}" .format(media))
+if media >= 10 :
+    print("Aluno aprovado")
+else:
+    print("Aluno reprovado")
+
+print("------------------------")
